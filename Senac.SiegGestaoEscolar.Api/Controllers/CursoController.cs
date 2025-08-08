29096f57bf61f42306be2df64c29c38c4d3f62ad
gabriel <gabrielh2007.scs@gmail.com>
2025-08-07 21:01:03 -0300

    Adicionando CursoController

--- Senac.SiegGestaoEscolar.Api/Controllers/CursoController.cs
+++ Senac.SiegGestaoEscolar.Api/Controllers/CursoController.cs
@@ -1,8 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Senac.SiegGestaoEscolar.Domain.Dtos.Erro;
-using Senac.SiegGestaoEscolar.Domain.Services.Aluno;
+using Senac.SiegGestaoEscolar.Domain.Dtos.Request.Curso;
 using Senac.SiegGestaoEscolar.Domain.Services.Curso;
-
 
 namespace Senac.SiegGestaoEscolar.Api.Http.Controllers;
 
@@ -42,4 +41,60 @@ public class CursoController : Controller
             return NotFound(erroResponse);
         }
     }
+
+    [HttpPut("/adicionar/curso")]
+    public async Task<IActionResult> AdicionarCurso([FromBody] AdicionarCursoRequest adicionarCursoRequest)
+    {
+        try
+        {
+            var adicionarCursoResponse = await _cursoService.AdicionarCurso(adicionarCursoRequest);
+            return Ok(adicionarCursoResponse);
+        }
+        catch (Exception ex)
+        {
+            var erroResponse = new ErroResponse
+            {
+                Mensagem = ex.Message,
+            };
+            return BadRequest(erroResponse);
+        }
+    }
+
+    [HttpPost("curso/{id}/atualizar/")]
+    public async Task<IActionResult> AtualizarCurso([FromRoute] long id, [FromBody] AtualizarCursoRequest atualizarCursoRequest)
+    {
+        try
+        {
+            await _cursoService.AtualizarCurso(id, atualizarCursoRequest);
+            return Ok();
+        }
+        catch (Exception ex)
+        {
+            var erroResponse = new ErroResponse
+            {
+                Mensagem = ex.Message,
+            };
+            return BadRequest(erroResponse);
+        }
+    }
+
+    [HttpDelete("curso/{id}/deletar")]
+    public async Task<IActionResult> DeletarCurso([FromRoute] long id)
+    {
+        try
+        {
+            await _cursoService.DeletarCurso(id);
+            return Ok();
+        }
+        catch (Exception ex)
+        {
+            var erroResponse = new ErroResponse
+            {
+                Mensagem = ex.Message,
+            };
+            return BadRequest(erroResponse);
+        }
+    }
+
 }
+
