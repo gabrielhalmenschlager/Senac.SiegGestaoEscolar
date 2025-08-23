4591d925b2da5abdcd86273b608ae4914ced88e9
“gabrielhalmenschlager” <gabrielh2007.scs@gmail.com>
2025-08-23 08:51:03 -0300

    Atualizações nao confirmadas

--- sieg-gestao-escolar-frontend/sieg-gestao-escolar-frontend/src/services/alunos.js
+++ sieg-gestao-escolar-frontend/sieg-gestao-escolar-frontend/src/services/alunos.js
@@ -1,11 +1,7 @@
 import api from './api';
 
 export async function obterTodosAlunos() {
-  const response = await api.get('/aluno', {
-  headers: {
-    'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJnYWJyaWVsLmhhbG1lbnNjaGxhZ2VyIiwianRpIjoiYWM0Yzk0MGQtZjJmOS00YmRlLTg3ZjktYmJlNWE0ZTA2OWM4IiwiZXhwIjoxNzU1MDQ1ODM1LCJpc3MiOiJodHRwczovL2lkZW50aXR5c2VydmVyLWFwaS5vbnJlbmRlci5jb20iLCJhdWQiOiJodHRwczovL2lkZW50aXR5c2VydmVyLWFwaS5vbnJlbmRlci5jb20ifQ.0Lcawg0wOxN8_UYr2mwrmocyGN_DPRUqMzfbrM4pgGs'
-  }
-})
+  const response = await api.get('/aluno',)
   return response.data;
 }
 
